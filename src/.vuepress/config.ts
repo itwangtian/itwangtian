cbb37e3cf8759d8742f28b836102c0dea5886a1b
小王在努力 <17638567073@163.com>
2023-09-16 18:07:37 +0800

    更新

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,12 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-
 export default defineUserConfig({
   base: "/wangtian/",
-
   lang: "zh-CN",
   title: "王天的web进阶之路",
   description: "前端编程技术分享 itwangtian",
  
   theme,
-
   // Enable it with pwa
   // shouldPrefetch: false,
 });
