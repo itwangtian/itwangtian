00091fcf1ed66bf3939c4d0a2ca33cd458003e02
小王在努力 <17638567073@163.com>
2023-09-16 18:01:39 +0800

    更改vuepressconfig配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/itwangtian/",
+  base: "/",
 
   lang: "zh-CN",
   title: "王天的web进阶之路",
