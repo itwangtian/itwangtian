45ebb6a4ff2002ae0137e74c6847ac8ca1b9a0df
小王在努力 <17638567073@163.com>
2023-09-16 18:22:42 +0800

    更新主题配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 export default defineUserConfig({
-  base: "/",
+  base: "/itwangtian/",
   lang: "zh-CN",
   title: "王天的web进阶之路",
   description: "前端编程技术分享 itwangtian",
