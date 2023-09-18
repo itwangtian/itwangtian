4a6798162120281126d64e7ec4103955f40d90bd
小王在努力 <17638567073@163.com>
2023-09-18 22:27:40 +0800

    vuepress配置百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,12 +2,24 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js"; 
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
+// 这里使用的是 vuepress主题 配置项
 export default defineUserConfig({
+   // 配置百度统计脚本，最终以script标签 插入header中
+  head: [
+    [
+      'script', {
+        type:'text/javascript',
+        src:'https://hm.baidu.com/hm.js?ff5e95667789a353e9be4e467e13ff3a'
+      }, 
+    ]
+  ],
   base: "/",
   lang: "zh-CN",
   title: "王天的web进阶之路",
   description: "前端编程技术分享 itwangtian",
   theme, 
+ 
+
   plugins: [
     searchProPlugin({
       // 索引全部内容
