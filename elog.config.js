05d613d996b16a1d454e1830b5f6f1be87d6bef6
小王在努力 <17638567073@163.com>
2023-10-13 22:58:22 +0800

    更新描述文档

--- elog.config.js
+++ elog.config.js
@@ -74,3 +74,33 @@ module.exports = {
     },
   },
 }
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const sliceName= createSlice({
+    name: "sliceName",
+    initialState: {},
+    reducers: {
+        invalidate: (state, { payload }) => {
+            payload.forEach(item => {
+                state[item] = false
+            })
+        }
+    },
+    extraReducers: builder => builder
+        .addCase(actionName.pending, (state, { payload }) => {
+            state.loading = true
+        })
+        .addCase(actionName.fulfilled, (state, { payload }) => {
+            state.loading = false
+        })
+        .addCase(actionName.rejected, (state, { payload }) => {
+            state.loading = false
+            state.error = payload
+        })
+       
+})
+
+export const { invalidate } = sliceName.actions
+export default sliceName.reducer
+
